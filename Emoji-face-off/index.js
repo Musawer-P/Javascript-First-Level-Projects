1c374851566ef2260ae8cb6ea18e2c009a4daa1c
Musawer-P <m.musawer.p@gmail.com>
2025-01-04 19:23:23 +0430

    Emoji Face off

---
+++ Emoji-face-off/index.js
@@ -0,0 +1,11 @@
+let fighters = ["🐉", "🐥", "🐊", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+
+let stageEl = document.getElementById("stage")
+let fightButton = document.getElementById("fightButton")
+
+fightButton.addEventListener("click", function() {
+  
+  let animal = Math.floor(Math.random() * fighters.length)
+  let animalTwo = Math.floor(Math.random() * fighters.length)
+  stageEl.textContent = (fighters[animal] + " vs " + fighters[animalTwo])
+  })
