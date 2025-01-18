5b3868b5e476fff3fd7865033dde80fea23a0b4e
Musawer-P <m.musawer.p@gmail.com>
2025-01-18 23:13:50 +0430

    adding database URL from firebase

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
 import { getDatabase,
          ref,
          push,
@@ -6,7 +6,7 @@ import { getDatabase,
          remove } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
 
 const firebaseConfig = {
-    databaseURL: "https://leads-tracker-app-default-rtdb.europe-west1.firebasedatabase.app/"
+    databaseURL: "https://leads-tracker-app-4ffb0-default-rtdb.firebaseio.com/"
 }
 
 const app = initializeApp(firebaseConfig)
