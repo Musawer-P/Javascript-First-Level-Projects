7f66a50fa261186daa23a3c65771cf385497b1f9
Musawer-P <m.musawer.p@gmail.com>
2025-01-18 23:47:10 +0430

    adding app link to README file

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"
 import { getDatabase,
          ref,
          push,
