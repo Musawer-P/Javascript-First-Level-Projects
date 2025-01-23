ea1058e7746214463ae80a34f6cb98835bf2b82b
Musawer-P <m.musawer.p@gmail.com>
2025-01-23 23:13:13 +0430

    saving inputs data into localStorage

--- localstorage-project/script.js
+++ localstorage-project/script.js
@@ -1,6 +1,17 @@
-let inputData = document.getElementById("save-form")
+let saveForm = document.getElementById("save-form");
+let quantityInput = document.getElementById("quantity");
+let itemInput = document.getElementById("item");
+let priceInput = document.getElementById("price");
 
-function submit(){
-    localStorage.setItem("data", "new");
+saveForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const quantity = quantityInput.value;
+    const item = itemInput.value;
+    const price = priceInput.value;
 
-}
+    localStorage.setItem("quantity", quantity);
+    localStorage.setItem("item", item);
+    localStorage.setItem("price", price);
+    
+     alert('Data saved to localStorage successfully!');
+});
