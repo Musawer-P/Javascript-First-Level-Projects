ffa77d10c2faf8491a79606aa8fc8382afb77886
Musawer-P <m.musawer.p@gmail.com>
2025-01-27 21:53:02 +0430

    fixing some errors in js code

--- To-Do-List-App/script.js
+++ To-Do-List-App/script.js
@@ -6,8 +6,9 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebas
          remove } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-database.js"
 
 const firebaseConfig = {
-    databaseURL: ""
+    databaseURL: "https://leads-tracker-app-4ffb0-default-rtdb.firebaseio.com/"
 }
+
 const app = initializeApp(firebaseConfig)
 const database = getDatabase(app)
 const referenceInDB = ref(database, "todo")
@@ -15,10 +16,11 @@ const referenceInDB = ref(database, "todo")
 const inputTodo = document.getElementById("input-todo")
 const submitTodo = document.getElementById("submit-todo")
 const insertplaceTodo = document.getElementById("insertplace-todo")
+const deleteBtn = document.getElementById("delete-btn")
 
 function render(todo){
     let listItems = ""
-    for(let i = 0; i < leads.length; i++){
+    for(let i = 0; i < todo.length; i++){
         listItems += `
         <li>
              <a target='_blank' href = '${todo[i]}'>
@@ -41,12 +43,12 @@ onValue(referenceInDB,function (snapshot){
     }
 })
 
-deleteBtn.addEventListenere("dblclick", function(){
+deleteBtn.addEventListener("dblclick", function(){
     remove(referenceInDB)
     insertplaceTodo.innerHTML = ""
 })
 
-submit-todo.addEventListenere("click" , function(){
-    push(referenceInDB,insertplaceTodo.value)
-    insertplaceTodo.value = ""
+submitTodo.addEventListener("click" , function(){
+    push(referenceInDB,inputTodo.value)
+    inputTodo.value = ""
 })
