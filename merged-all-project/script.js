7c610a6a31b016fdfdcd92d1389cff2e9f12c82a
Musawer-P <m.musawer.p@gmail.com>
2025-05-12 23:57:54 +0430

    updating the code and merging two project

--- merged-all-project/script.js
+++ merged-all-project/script.js
@@ -1,3 +1,4 @@
+ // Main page
  const cards = document.querySelectorAll('.card');
   let currentIndex = 1; // Start with center one active
 
@@ -14,3 +15,74 @@
     currentIndex = (currentIndex + direction + cards.length) % cards.length;
     updateCards();
   }
+// Done
+
+  // Clingy Popup
+const mainModal = document.getElementById("main")
+const popupModal = document.getElementById("popup")
+const popupText = document.getElementById("inside-text")
+const loginForm = document.getElementById("login-form")
+const closeBtn = document.getElementById("close-btn")
+const emailI = document.getElementById("email")
+const acceptBtn = document.getElementById("accept-btn")
+const declineBtn = document.getElementById("decline-btn")
+const button = document.getElementById("button")
+
+setTimeout(function(){
+popupModal.style.display = 'block'
+},1500)
+
+closeBtn.addEventListener('click', function(){
+    popupModal.style.display = 'none'
+})
+
+
+loginForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    popupText.innerHTML = `<div class="modal-inner-loading">
+        <img src="images/loading.gif" class="loading">
+        <p id="uploadText">
+            Uploading your data to the dark web...
+        </p>
+    </div>`;
+
+    setTimeout(function() {
+        document.getElementById('uploadText').innerText = `Making the sale...`; 
+    }, 1500); 
+
+    setTimeout(function(){
+        document.getElementById('popup').innerHTML = `<h2 style = "color:white;">Thanks <span class="modal-display-name">${fullName}</span>, you easy target! </h2>
+        <p style = "color:white;">We just sold the rights to your eternal soul.</p>
+        <div class="idiot-gif">
+            <img src="images/pirate.gif">
+        </div>`; 
+        closeBtn.disabled = false
+    },3000)
+
+    const formData = new FormData(loginForm)
+const fullName = formData.get('name')
+});
+
+declineBtn.addEventListener('mouseenter', function(){
+    button.classList.toggle('reverse')
+})
+// Done
+
+// Counter App
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+// Done 
