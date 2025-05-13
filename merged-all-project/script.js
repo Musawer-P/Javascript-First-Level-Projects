165fea3d92155d91d1074ac01bf406ce065ce656
Musawer-P <m.musawer.p@gmail.com>
2025-05-13 23:33:55 +0430

    updating the code

--- merged-all-project/script.js
+++ merged-all-project/script.js
@@ -85,4 +85,18 @@ function save() {
     countEl.textContent = 0
     count = 0
 }
-// Done 
+//Done 
+//Emoji Face off
+let fighters = ["🐉", "🐥", "🐊", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+
+let stageEl = document.getElementById("stage")
+let fightButton = document.getElementById("fightButton")
+
+fightButton.addEventListener("click", function() {
+  
+  let animal = Math.floor(Math.random() * fighters.length)
+  let animalTwo = Math.floor(Math.random() * fighters.length)
+  stageEl.textContent = (fighters[animal] + " vs " + fighters[animalTwo])
+  })
+
+//Done
